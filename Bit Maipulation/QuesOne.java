22029a99403f5e1c5eaf2e4ad6541b48470206a7
Ekta Mhaske <ektamhaske19@gmail.com>
2025-07-03 14:44:10 +0530

    Code Updated

---
+++ Bit Maipulation/QuesOne.java
@@ -0,0 +1,49 @@
+
+import java.util.Scanner;
+
+public class QuesOne {
+
+    // Calculate odd and Even 
+
+    public  static  void oddEven(int n) {
+        int Mask = 1;
+        if ((n & Mask) == 0) {
+            System.out.println("Even Number ");
+        } else {
+            System.out.println("Odd Number");
+        }
+        
+    }
+
+
+    // Get ith Bit
+
+    public static int getIthBit(int n, int i) {
+        int bitMask = 1<<i;
+        if ((n& bitMask ) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    // Set ith Bit 
+
+    public static int setIthBit(int n , int i ) {
+        int bitMask = 1<<i;
+        return n| bitMask;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no : ");
+        int n = sc.nextInt();
+        System.out.print("Enter i : ");
+        int i = sc.nextInt();
+        //oddEven(n);
+        //System.out.println(getIthBit(n, 2));
+        System.out.println(setIthBit(n, i));
+
+     }
+}
