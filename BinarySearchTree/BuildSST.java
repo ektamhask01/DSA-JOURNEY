d95ca4185fbd491f6f1f5d3a10a38775827ca7b5
Ekta Mhaske <ektamhaske19@gmail.com>
2026-01-22 17:54:17 +0530

    Binary Search Tree Updated

--- BinarySearchTree/BuildSST.java
+++ BinarySearchTree/BuildSST.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+
+
 public class BuildSST {
     static class Node {
         int data ;
@@ -97,19 +100,70 @@ public class BuildSST {
     }
 
 
+    // Question  1 : Print In range
+
+    public static void printInRange(Node root, int X , int Y) {
+        if (root == null ) {
+            return;
+        }
+        if (root.data >= X && root.data <=Y) {
+            printInRange(root.left, X, Y);
+            System.out.print(root.data + " ");
+            printInRange(root.right, X, Y);
+
+        } 
+
+        else if (root.data >= Y) {
+            printInRange(root.left, X, Y);
+        } else {
+            printInRange(root.right, X, Y);
+        }
+    }
+
+
+    // Question 2 : Root To Leaf Paths
+
+    public static void printRoot2Leaf(Node root ,ArrayList<Integer> path  ) {
+        if (root == null ) {
+            return;
+        }
+        path.add(root.data);
+
+        if (root.left == null && root.right == null) {
+            // Leaf Node
+            printPath(path);
+        } else {
+            printRoot2Leaf(root.left, path);
+            printRoot2Leaf(root.right, path);
+        }
+        path.remove(path.size()- 1);
+    }
+
+    public static void printPath(ArrayList<Integer> path) {
+        for (int i = 0 ; i<path.size();i++) {
+            System.out.print(path.get(i) + " -> ");
+        }
+        System.out.println();
+    }
+
 
     public static void main(String[] args) {
-        int values[] = {8, 5, 6, 3, 1, 4, 10, 11, 14};
+        int values[] = {8, 5, 6, 3, 10, 11, 14};
+        
         Node root = null;
         int key = 2;
         for (int i =0 ; i<values.length;i++) {
             root = insert(root, values[i]);
         }
-        inorder(root);
-        System.out.println();
-        System.out.println(search(root, key));
-        delete(root, 8);
-        inorder(root);
+        // inorder(root);
+        // System.out.println();
+        // System.out.println(search(root, key));
 
+        //printInRange(root, 3 ,12);
+
+        printRoot2Leaf(root, new ArrayList<>());
     }
+
+
+    
 }
